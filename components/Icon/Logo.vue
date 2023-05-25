75d736f0272dc38479c168c2fce3b55c1eac36f4
Affiong Udokisai <isaac.aku@cyphercrescent.com>
2023-05-25 08:56:22 +0100

    Latest

---
+++ components/Icon/Logo.vue
@@ -0,0 +1,16 @@
+<template>
+	<nav 
+        flex="~ row" px5 py5 lg:px44 xl:px64 justify-between items-center transition  
+        :class="'lg:py32'"
+    >
+        <NuxtLink to="/" tag="a">
+            <img 
+                h6 transition-height duration-400
+                :src=" '/triple-helix-logo2.png'" 
+                alt="Triple Helix Logo"
+                :class=" 'lg:h24' "
+            >
+        </NuxtLink>
+		</nav>
+	 
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+	<div class="min-h-screen relative">
+		<img
+		
+			src="/pic1.jpg"
+			alt="Advance Home"
+			class="object-cover h-50 w-50 absolute inset-0 -z-0"
+		/>
+		<div
+			class="absolute inset-0 bg-gradient-to-b from-white via-white/60 to-transparent"
+		></div>
+		<div class="h-full relative">
+			<Header />
+			<Hero />
+		</div>
+	</div>
+</template>
+
+<style></style>
